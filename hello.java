a3d79ebb5b29a204f9df9ba112cf53dbb132fab7
Sahu-ext <Anup.Sahu-ext@bshg.com>
2023-03-21 12:25:57 +0530

    'changed'

--- hello.java
+++ hello.java
@@ -1,9 +1,7 @@
-public class Hello{
-
-public static void main(String[] [args]){
-
-
-system.out.println("Hello world");
-}
-
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello");
+    }
 }
